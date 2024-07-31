5ea0e16bcbb1297201392e0549fb407869bf3aaf
alivaliyevprouser1 <ali.valiyev.dev@gmail.com>
2024-07-31 19:10:48 +0400

    fix: adjust positioning of decorative wavy lines image in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,14 +7,14 @@ import ShowcaseImage from "./ShowcaseImage";
 const About = () => {
   return (
     <section className="relative px-10 xl:px-32 mt-16 xl:mt-32 max-lg:gap-12 flex flex-col lg:flex-row xl:justify-between">
-      <div className="leftside relative z-30 flex justify-between">
+      <div className="relative z-30 flex justify-between">
         <ShowcaseImage
           mirror={false}
           imgSrc="/showcase-1.png"
         />
 
         <img
-          className={"sm:hidden"}
+          className="sm:hidden"
           src="/star-mark-2.svg"
           alt="Star mark"
           width={100}
@@ -22,7 +22,7 @@ const About = () => {
         />
       </div>
 
-      <div className="rightside flex flex-col justify-center relative z-30 px-0 md:px-6 xl:px-0 lg:max-w-[457px]">
+      <div className="flex flex-col justify-center relative z-30 px-0 md:px-6 xl:px-0 lg:max-w-[457px]">
         <SectionTitle title={titles[1]} />
         <div className="my-9 lg:my-14">
           <SectionSubtitle subtitle={subtitles[1]} />
@@ -49,7 +49,7 @@ const About = () => {
       </div>
 
       <img
-        className="max-sm:hidden absolute top-0 right-0 z-10 "
+        className="max-sm:hidden absolute top-40 right-0 z-10 "
         src="/wavy-lines-2-desktop.svg"
         alt="Decorative Wavy Lines"
       />

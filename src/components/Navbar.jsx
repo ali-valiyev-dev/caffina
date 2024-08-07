0bb02e3914bea05ce816bf726a763fa7d61f6bd0
alivaliyevprouser1 <ali.valiyev.dev@gmail.com>
2024-08-07 14:13:26 +0400

    feat: enhance components with updated styling and structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,64 +4,121 @@ import Logo from "./Logo";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleSmoothScroll = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    setIsOpen(false);
+  };
+
   return (
-    <nav className="flex max-lg:justify-between items-center pt-6 lg:pt-12 px-6 lg:px-10 xl:px-32 gap-24">
+    <nav className="borders flex max-lg:justify-between items-center mt-6 lg:mt-12 px-6 lg:px-10 xl:px-32 gap-24">
       <Logo />
-      {/* Desktop view Navbar */}
+      {/* Desktop Navbar */}
       <ul className="max-lg:hidden flex items-center justify-center gap-24 text-lg font-normal text-[#d9d9d9]">
         <li>
           <a
             className="hover:text-[#35C66B] transition-all duration-300"
-            href="#">
-            Home
+            href="#about"
+            onClick={e => handleSmoothScroll(e, "about")}>
+            How It&apos;s Made
           </a>
         </li>
         <li>
           <a
             className="hover:text-[#35C66B] transition-all duration-300"
-            href="#">
-            Select
+            href="#products"
+            onClick={e => handleSmoothScroll(e, "products")}>
+            Products
           </a>
         </li>
         <li>
           <a
             className="hover:text-[#35C66B] transition-all duration-300"
-            href="#">
-            Shop
+            href="#events"
+            onClick={e => handleSmoothScroll(e, "events")}>
+            Events
+          </a>
+        </li>
+        <li>
+          <a
+            className="hover:text-[#35C66B] transition-all duration-300"
+            href="#contacts"
+            onClick={e => handleSmoothScroll(e, "contacts")}>
+            Contact Us
           </a>
         </li>
       </ul>
 
-      {/* Mobile view Navbar */}
+      {/* Mobile Navbar */}
       <button
         type="button"
-        className="lg:hidden w-10 h-4"
+        className="lg:hidden w-10 relative"
         onClick={() => setIsOpen(!isOpen)}>
-        <img
-          src="/hamburger-menu.svg"
-          alt="hamburger menu"
-        />
+        <span
+          className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${
+            isOpen ? "opacity-100" : "opacity-0"
+          }`}>
+          <span className="text-[#237249] leading-none font-bold text-3xl">
+            &#10005;
+          </span>
+        </span>
+        <span
+          className={`absolute inset-0 flex items-center justify-center transition-opacity duration-300 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}>
+          <img
+            src="/hamburger-menu.svg"
+            alt="hamburger menu"
+          />
+        </span>
       </button>
 
       <div
-        className={`absolute left-0 top-14 bg-[#121517] w-full overflow-hidden transition-max-height duration-500 ease-in-out z-50 ${
+        className={`absolute left-0 top-14 md:top-16 bg-[#121517] w-full overflow-hidden transition-max-height duration-500 ease-in-out z-50 ${
           isOpen ? "max-h-screen border-[#35C66B] border-b" : "max-h-0"
         }`}>
         <ul className="flex flex-col items-center justify-center my-10 text-xl font-normal text-[#d9d9d9]">
           <li
             className="w-full py-7 text-center active:bg-[#35C66B]"
             onClick={() => setIsOpen(!isOpen)}>
-            <a href="#">Home</a>
+            <a
+              href="#about"
+              onClick={e => handleSmoothScroll(e, "about")}>
+              How It&apos;s Made
+            </a>
           </li>
           <li
             className="w-full py-7 text-center active:bg-[#35C66B]"
             onClick={() => setIsOpen(!isOpen)}>
-            <a href="#">Select</a>
+            <a
+              href="#products"
+              onClick={e => handleSmoothScroll(e, "products")}>
+              Products
+            </a>
           </li>
           <li
             className="w-full py-7 text-center active:bg-[#35C66B]"
             onClick={() => setIsOpen(!isOpen)}>
-            <a href="#">Shop</a>
+            <a
+              href="#events"
+              onClick={e => handleSmoothScroll(e, "events")}>
+              Events
+            </a>
+          </li>
+          <li
+            className="w-full py-7 text-center active:bg-[#35C66B]"
+            onClick={() => setIsOpen(!isOpen)}>
+            <a
+              href="#contacts"
+              onClick={e => handleSmoothScroll(e, "contacts")}>
+              Contact Us
+            </a>
           </li>
         </ul>
       </div>
